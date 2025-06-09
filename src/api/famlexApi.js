179fc3d9ff057328502010fbc2585248394fb902
Jaquecege <yolala1712@gmail.com>
2025-06-09 15:26:37 -0600

    Corrijo rutas POST con /api y payload con username

--- src/api/famlexApi.js
+++ src/api/famlexApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 export const registrarUsuario = (email, password) =>
-  API.post("/register", { email, password });
+  API.post("/register", { username: email, password });
 
 export const iniciarSesion = (email, password) =>
-  API.post("/login", { email, password });
+  API.post("/login", { username: email, password });
