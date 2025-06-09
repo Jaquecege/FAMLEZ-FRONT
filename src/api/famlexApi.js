0d32c023c6fbeef2b2052eecc180bfb4e4239f34
Jaquecege <yolala1712@gmail.com>
2025-06-09 15:13:42 -0600

    Agrego variable VITE_API_URL y ajustes de conexión con backend

--- src/api/famlexApi.js
+++ src/api/famlexApi.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
-// Registro
 export const registrarUsuario = (email, password) =>
   API.post("/register", { email, password });
 
-// Login
 export const iniciarSesion = (email, password) =>
   API.post("/login", { email, password });
